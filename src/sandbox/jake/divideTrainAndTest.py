7f38de32c201f2780fd930eb0688138f372e052a
Jakob Sahlström <jakob.sahlstrom@gmail.com>
2014-05-08 15:13:35 +0200

    Refactoring code

--- src/sandbox/jake/divideTrainAndTest.py
+++ src/sandbox/jake/divideTrainAndTest.py
@@ -1,66 +1,64 @@
 import json
-import os.path
-import string
 import random as rand
-import os, errno
+import os
+import errno
 from os import listdir
 
-def divideToTrainAndTestSets():
-	parentDir = os.path.abspath(os.path.join(__file__, os.pardir)) 	# sandbox
-	parentDir = os.path.abspath(os.path.join(parentDir, os.pardir)) # src
-	parentDir = os.path.abspath(os.path.join(parentDir, os.pardir)) # MLProject
-	parentDir = os.path.abspath(os.path.join(parentDir, os.pardir)) # MLProject
-	jsonDir = parentDir + '/res/articles/'
 
-	articleFiles = []
-	fileEnding = "-articles.json"
-	for fileName in listdir(jsonDir):
-		if fileName.endswith(fileEnding):
-			articleFiles.append(fileName)
+def dividetotrainandtestsets():
+	parentdir = os.path.abspath(os.path.join(__file__, os.pardir))  # sandbox
+	parentdir = os.path.abspath(os.path.join(parentdir, os.pardir))  # src
+	parentdir = os.path.abspath(os.path.join(parentdir, os.pardir))  # MLProject
+	parentdir = os.path.abspath(os.path.join(parentdir, os.pardir))  # MLProject
+	jsondir = parentdir + '/res/articles/'
+
+	articlefiles = []
+	fileending = "-articles.json"
+	for fileName in listdir(jsondir):
+		if fileName.endswith(fileending):
+			articlefiles.append(fileName)
 		#endif
 	#endfor
 
+	chancetotest = 0.33
 
-	chanceToTest = 0.33
-	ctr = 0
-
-	for fileName in articleFiles:
-		testArticles = []
-		trainArticles = []
+	for fileName in articlefiles:
+		testarticles = []
+		trainarticles = []
 		# Open JSON file
-		json_data = open(jsonDir + fileName)
+		json_data = open(jsondir + fileName)
 		data = json.load(json_data)
 		json_data.close()
 		for artObj in data:
 			# artObj["article"] = filter(lambda x: x in string.printable, artObj["article"])
 			# artObj["title"] = filter(lambda x: x in string.printable, artObj["title"])
-			if rand.random() < chanceToTest:
-				testArticles.append(artObj)
+			if rand.random() < chancetotest:
+				testarticles.append(artObj)
 			else:
-				trainArticles.append(artObj)
+				trainarticles.append(artObj)
 			#endif
 		#endfor
 
-		with open(jsonDir + 'training_data/'+fileName, 'w') as trainf:
-			json.dump(trainArticles, trainf,indent=4, sort_keys=True)
-			# print json.dumps(trainArticles, indent=4, sort_keys=True)
+		with open(jsondir + 'training_data/' + fileName, 'w') as trainf:
+			json.dump(trainarticles, trainf, indent=4, sort_keys=True)
 		#endwith 
 
-		with open(jsonDir + 'test_data/'+fileName, 'w') as testf:
-			json.dump(testArticles, testf,indent=4, sort_keys=True)
-			# print json.dumps(testArticles, indent=4, sort_keys=True)
-		#endwith 
-	#endfor
+		with open(jsondir + 'test_data/' + fileName, 'w') as testf:
+			json.dump(testarticles, testf, indent=4, sort_keys=True)
+		#endwith
+		#endfor
+
 
 def silentremove(filename):
 	try:
 		os.remove(filename)
 	except OSError as e:
-		if e.errno != errno.ENOENT: # errno.ENOENT = no such file or directory
-			raise # re-raise exception if a different error occure
+		if e.errno != errno.ENOENT:  # errno.ENOENT = no such file or directory
+			raise  # re-raise exception if a different error occure
+
 
 def main():
-	divideToTrainAndTestSets()
+	dividetotrainandtestsets()
 
 # Standard boilerplate to call the main() function to begin
 # the program.
