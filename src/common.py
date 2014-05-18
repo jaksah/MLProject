3eb20f37e74ce8e7709cc677cd914d66adab6785
Jakob Sahlström <jakob.sahlstrom@gmail.com>
2014-05-18 13:25:26 +0200

    Added stemming to article parsing

--- src/common.py
+++ src/common.py
@@ -1,14 +1,21 @@
-import tokenize
 import re
 from sets import Set
+from stemming.porter2 import stem
+
 
 # Convert article to set that is compatible with vocabulary
 def articleToSet(article):
-	article = article.split( )
+	article = article.split()
+	article = Set(article)  # Remove duplicates before cleaning
 	article = [x.lower() for x in article]
 	article = [re.sub(r"[^a-z-]","",x) for x in article]
 	article = [re.sub(r"--+","",x) for x in article]
 	article = [re.sub(r"^-","",x) for x in article]
-	articleSet = Set(article)
-	return articleSet
+	article = [stemword(x) for x in article]
+	articleset = Set(article)  # Remove duplicates after stemming
+	return articleset
 # end def
+
+
+def stemword(aword):
+	return stem(aword)
